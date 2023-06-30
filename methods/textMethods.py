43ca484e229119ffc836f72f8e6108b543e37320
Yassine <ykhattabi@eig.fr>
2023-06-30 16:56:51 +0200

    commit

--- methods/textMethods.py
+++ methods/textMethods.py
@@ -55,3 +55,6 @@ class textMethods:
     
     def stringOptions(self, word: str):
         return [word.upper(),word.lower(), word.capitalize()]
+    
+    def printMessage(self):
+        print(textMethods.message)
